43aca6b349638b6d4912078fc65c3a799113a00d
suthirakprom <suthirakprom81@gmail.com>
2019-07-23 11:34:44 +0700

    creating menu

--- creatingmenu/app/src/main/java/com/example/creatingmenu/MainActivity.kt
+++ creatingmenu/app/src/main/java/com/example/creatingmenu/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.creatingmenu
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,4 +19,34 @@ class MainActivity : AppCompatActivity() {
         menuInflater.inflate(R.menu.my_menu,menu)
         return super.onCreateOptionsMenu(menu)
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId)
+        {
+            R.id.menu1 -> {
+                Toast.makeText(this, "Pizza", Toast.LENGTH_SHORT).show()
+                var pizza = Intent(this, Main2Activity::class.java)
+                startActivity(pizza)
+            }
+            R.id.menu2 -> {
+                Toast.makeText(this, "Buger", Toast.LENGTH_SHORT).show()
+                var burger = Intent(this, Main3Activity::class.java)
+                startActivity(burger)
+            }
+            R.id.menu3 -> {
+                Toast.makeText(this, "Salad", Toast.LENGTH_SHORT).show()
+                var salad = Intent(this, Main4Activity::class.java)
+                startActivity(salad)
+            }
+            R.id.menu4 -> {
+                Toast.makeText(this, "Chicken", Toast.LENGTH_SHORT).show()
+                var chicken = Intent(this, Main5Activity::class.java)
+                startActivity(chicken)
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
 }
