a11565645043c1847ac46e6bdfdd5c7160e83edf
suthirakprom <suthirakprom81@gmail.com>
2019-07-23 10:42:51 +0700

    adding

--- passImageToAnotherActivity/app/src/main/java/com/example/passimagetoanotheractivity/MainActivity.kt
+++ passImageToAnotherActivity/app/src/main/java/com/example/passimagetoanotheractivity/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    var images = arrayOf(R)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
